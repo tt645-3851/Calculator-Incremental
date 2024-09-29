75dd81b9d2f64bf13959704f61552d260c83505e
tt645-3851 <tt645@drexel.edu>
2024-09-29 05:37:01 -0400

    Reimplemented functionality for the clear button

--- calculator.js
+++ calculator.js
@@ -50,13 +50,26 @@ keys.addEventListener('click', e => {
             }
             calculator.dataset.previousKeyType = 'decimal';
         }
+        if (action !== 'clear') {
+            const clearButton = calculator.querySelector('[data-action=clear]');
+            clearButton.textContent = 'CE';
+        }
         if (action === 'clear') {
+            if (key.textContent === 'AC') {
+                calculator.dataset.firstValue = '';
+                calculator.dataset.modValue = '';
+                calculator.dataset.operator = '';
+                calculator.dataset.previousKeyType = '';
+            } else {
+                key.textContent = 'AC';                
+            }
+            display.textContent = 0;
             calculator.dataset.previousKeyType = 'clear';
         }
         if (action === 'calculate') {
             let firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
             let secondValue = displayedNum;
-            let operator = calculator.dataset.operator;
 
             if (firstValue) {
                 if (previousKeyType === 'calculate') {
